c9cadd32bc4d065f7ddbbb3654006184996b5ee6
Oleg Skarednov <olegskar1@gmail.com>
2022-04-25 18:52:05 +0500

    fix:header

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-'use strict';
+"use strict";
 (function () {
-  const header = document.querySelector('.header');
+  const header__container = document.querySelector(".header__container");
   window.onscroll = () => {
     if (window.scrollY > 75) {
-      header.classList.add('header_active');
-    } else header.classList.remove('header_active');
+      header__container.classList.add("header_active");
+    } else header__container.classList.remove("header_active");
   };
 })();
 
 //Burger Handler
 (function () {
-  const burgerItem = document.querySelector('.burger');
-  const menu = document.querySelector('nav');
-  const menuCloseItem = document.querySelector('.header__nav-close');
-  burgerItem.addEventListener('click', () => {
-    menu.classList.add('nav_active');
+  const burgerItem = document.querySelector(".burger");
+  const menu = document.querySelector("nav");
+  const menuCloseItem = document.querySelector(".header__nav-close");
+  burgerItem.addEventListener("click", () => {
+    menu.classList.add("nav_active");
   });
-  menuCloseItem.addEventListener('click', () => {
-    menu.classList.remove('nav_active');
+  menuCloseItem.addEventListener("click", () => {
+    menu.classList.remove("nav_active");
   });
 })();

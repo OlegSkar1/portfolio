5c24d9235a9a3b7f202a7b572cf8f29069533db5
Oleg Skarednov <olegskar1@gmail.com>
2022-05-10 17:40:33 +0500

    feat: changeURLLanguage

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-'use strict';
+"use strict";
 (function () {
-  const header__container = document.querySelector('.header__container');
+  const header__container = document.querySelector(".header__container");
   window.onscroll = () => {
     if (window.scrollY > 75) {
-      header__container.classList.add('header_active');
-    } else header__container.classList.remove('header_active');
+      header__container.classList.add("header_active");
+    } else header__container.classList.remove("header_active");
   };
 })();
 
 //Burger Handler
 (function () {
-  const burgerItem = document.querySelector('.burger');
-  const menu = document.querySelector('nav');
-  const menuCloseItem = document.querySelector('.header__nav-close');
-  const navCloseList = document.querySelectorAll('.nav-list__link');
-  burgerItem.addEventListener('click', () => {
-    menu.classList.add('nav_active');
+  const burgerItem = document.querySelector(".burger");
+  const menu = document.querySelector("nav");
+  const menuCloseItem = document.querySelector(".header__nav-close");
+  const navCloseList = document.querySelectorAll(".nav-list__link");
+  burgerItem.addEventListener("click", () => {
+    menu.classList.add("nav_active");
   });
-  menuCloseItem.addEventListener('click', () => {
-    menu.classList.remove('nav_active');
+  menuCloseItem.addEventListener("click", () => {
+    menu.classList.remove("nav_active");
   });
   for (let item of navCloseList) {
-    item.addEventListener('click', () => {
-      menu.classList.remove('nav_active');
+    item.addEventListener("click", () => {
+      menu.classList.remove("nav_active");
     });
   }
 })();
+
+//start translate
+const select = document.querySelectorAll(".header__currLang");
+
+for (let lang of select) {
+  lang.addEventListener("click", (event) => {
+    let currLang = event.target.innerHTML;
+    location.href = window.location.pathname + "#" + currLang;
+  });
+}

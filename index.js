7465471f475a2cb67f04955795f5712ca6b7159f
Oleg Skarednov <olegskar1@gmail.com>
2022-05-16 21:03:11 +0500

    feat: button animation

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-"use strict";
+'use strict';
 (function () {
-  const header__container = document.querySelector(".header__container");
+  const header__container = document.querySelector('.header__container');
   window.onscroll = () => {
     if (window.scrollY > 75) {
-      header__container.classList.add("header_active");
-    } else header__container.classList.remove("header_active");
+      header__container.classList.add('header_active');
+    } else header__container.classList.remove('header_active');
   };
 })();
 
 //Burger Handler
 (function () {
-  const burgerItem = document.querySelector(".burger");
-  const menu = document.querySelector("nav");
-  const menuCloseItem = document.querySelector(".header__nav-close");
-  const navCloseList = document.querySelectorAll(".nav-list__link");
-  burgerItem.addEventListener("click", () => {
-    menu.classList.add("nav_active");
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('nav');
+  const menuCloseItem = document.querySelector('.header__nav-close');
+  const navCloseList = document.querySelectorAll('.nav-list__link');
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('nav_active');
   });
-  menuCloseItem.addEventListener("click", () => {
-    menu.classList.remove("nav_active");
+  menuCloseItem.addEventListener('click', () => {
+    menu.classList.remove('nav_active');
   });
   for (let item of navCloseList) {
-    item.addEventListener("click", () => {
-      menu.classList.remove("nav_active");
+    item.addEventListener('click', () => {
+      menu.classList.remove('nav_active');
     });
   }
 })();
 
 //translate
-let languageTranslate = "en";
-const form = document.querySelector(".contacts-form");
-const tel = document.getElementById("tel");
-const message = document.getElementById("message");
+let languageTranslate = 'en';
+const form = document.querySelector('.contacts-form');
+const tel = document.getElementById('tel');
+const message = document.getElementById('message');
 
 function changeTranslate(language) {
   if (language === null) return;
 
   languageTranslate = language;
-  localStorage.setItem("lang", languageTranslate);
+  localStorage.setItem('lang', languageTranslate);
 
-  if (language === "ru") {
-    lang_en.classList.remove("changeColor");
-    lang_ru.classList.add("changeColor");
-    tel.placeholder = "Телефон";
-    message.placeholder = "Сообщение";
+  if (language === 'ru') {
+    lang_en.classList.remove('changeColor');
+    lang_ru.classList.add('changeColor');
+    tel.placeholder = 'Телефон';
+    message.placeholder = 'Сообщение';
   } else {
-    lang_ru.classList.remove("changeColor");
-    lang_en.classList.add("changeColor");
-    tel.placeholder = "Phone";
-    message.placeholder = "Message";
+    lang_ru.classList.remove('changeColor');
+    lang_en.classList.add('changeColor');
+    tel.placeholder = 'Phone';
+    message.placeholder = 'Message';
   }
-  Array.from(document.getElementsByTagName("*")).forEach((item) => {
+  Array.from(document.getElementsByTagName('*')).forEach((item) => {
     if (
       item.dataset.translate &&
       i18Obj[languageTranslate][item.dataset.translate]
@@ -60,103 +60,123 @@ function changeTranslate(language) {
   });
 }
 document.addEventListener(
-  "DOMContentLoaded",
-  changeTranslate(localStorage.getItem("lang"))
+  'DOMContentLoaded',
+  changeTranslate(localStorage.getItem('lang'))
 );
 //Portfolio start
-const portfolio = document.querySelector(".portfolio__btns");
-let portfolioPics = document.querySelectorAll(".portfolio__pic__thumb");
-const button = document.getElementsByTagName("button");
+const portfolio = document.querySelector('.portfolio__btns');
+let portfolioPics = document.querySelectorAll('.portfolio__pic__thumb');
+const button = document.getElementsByTagName('button');
 
-portfolio.addEventListener("click", (event) => {
+portfolio.addEventListener('click', (event) => {
   event.preventDefault();
   switch (event.target) {
     case button.winter:
       {
-        pic1.setAttribute("src", "./assets/img/winter/1.jpg");
-        pic2.setAttribute("src", "./assets/img/winter/2.jpg");
-        pic3.setAttribute("src", "./assets/img/winter/3.jpg");
-        pic4.setAttribute("src", "./assets/img/winter/4.jpg");
-        pic5.setAttribute("src", "./assets/img/winter/5.jpg");
-        pic6.setAttribute("src", "./assets/img/winter/6.jpg");
-        winter.classList.add("portfolio__btn_changeColor");
-        autumn.classList.remove("portfolio__btn_changeColor");
-        spring.classList.remove("portfolio__btn_changeColor");
-        summer.classList.remove("portfolio__btn_changeColor");
+        pic1.setAttribute('src', './assets/img/winter/1.jpg');
+        pic2.setAttribute('src', './assets/img/winter/2.jpg');
+        pic3.setAttribute('src', './assets/img/winter/3.jpg');
+        pic4.setAttribute('src', './assets/img/winter/4.jpg');
+        pic5.setAttribute('src', './assets/img/winter/5.jpg');
+        pic6.setAttribute('src', './assets/img/winter/6.jpg');
+        winter.classList.add('portfolio__btn_changeColor');
+        autumn.classList.remove('portfolio__btn_changeColor');
+        spring.classList.remove('portfolio__btn_changeColor');
+        summer.classList.remove('portfolio__btn_changeColor');
       }
       break;
     case button.spring:
       {
-        pic1.setAttribute("src", "./assets/img/spring/1.jpg");
-        pic2.setAttribute("src", "./assets/img/spring/2.jpg");
-        pic3.setAttribute("src", "./assets/img/spring/3.jpg");
-        pic4.setAttribute("src", "./assets/img/spring/4.jpg");
-        pic5.setAttribute("src", "./assets/img/spring/5.jpg");
-        pic6.setAttribute("src", "./assets/img/spring/6.jpg");
-        spring.classList.add("portfolio__btn_changeColor");
-        winter.classList.remove("portfolio__btn_changeColor");
-        autumn.classList.remove("portfolio__btn_changeColor");
-        summer.classList.remove("portfolio__btn_changeColor");
+        pic1.setAttribute('src', './assets/img/spring/1.jpg');
+        pic2.setAttribute('src', './assets/img/spring/2.jpg');
+        pic3.setAttribute('src', './assets/img/spring/3.jpg');
+        pic4.setAttribute('src', './assets/img/spring/4.jpg');
+        pic5.setAttribute('src', './assets/img/spring/5.jpg');
+        pic6.setAttribute('src', './assets/img/spring/6.jpg');
+        spring.classList.add('portfolio__btn_changeColor');
+        winter.classList.remove('portfolio__btn_changeColor');
+        autumn.classList.remove('portfolio__btn_changeColor');
+        summer.classList.remove('portfolio__btn_changeColor');
       }
       break;
     case button.summer:
       {
-        pic1.setAttribute("src", "./assets/img/summer/1.jpg");
-        pic2.setAttribute("src", "./assets/img/summer/2.jpg");
-        pic3.setAttribute("src", "./assets/img/summer/3.jpg");
-        pic4.setAttribute("src", "./assets/img/summer/4.jpg");
-        pic5.setAttribute("src", "./assets/img/summer/5.jpg");
-        pic6.setAttribute("src", "./assets/img/summer/6.jpg");
-        summer.classList.add("portfolio__btn_changeColor");
-        winter.classList.remove("portfolio__btn_changeColor");
-        spring.classList.remove("portfolio__btn_changeColor");
-        autumn.classList.remove("portfolio__btn_changeColor");
+        pic1.setAttribute('src', './assets/img/summer/1.jpg');
+        pic2.setAttribute('src', './assets/img/summer/2.jpg');
+        pic3.setAttribute('src', './assets/img/summer/3.jpg');
+        pic4.setAttribute('src', './assets/img/summer/4.jpg');
+        pic5.setAttribute('src', './assets/img/summer/5.jpg');
+        pic6.setAttribute('src', './assets/img/summer/6.jpg');
+        summer.classList.add('portfolio__btn_changeColor');
+        winter.classList.remove('portfolio__btn_changeColor');
+        spring.classList.remove('portfolio__btn_changeColor');
+        autumn.classList.remove('portfolio__btn_changeColor');
       }
       break;
     case button.autumn: {
-      pic1.setAttribute("src", "./assets/img/autumn/1.jpg");
-      pic2.setAttribute("src", "./assets/img/autumn/2.jpg");
-      pic3.setAttribute("src", "./assets/img/autumn/3.jpg");
-      pic4.setAttribute("src", "./assets/img/autumn/4.jpg");
-      pic5.setAttribute("src", "./assets/img/autumn/5.jpg");
-      pic6.setAttribute("src", "./assets/img/autumn/6.jpg");
-      autumn.classList.add("portfolio__btn_changeColor");
-      winter.classList.remove("portfolio__btn_changeColor");
-      spring.classList.remove("portfolio__btn_changeColor");
-      summer.classList.remove("portfolio__btn_changeColor");
+      pic1.setAttribute('src', './assets/img/autumn/1.jpg');
+      pic2.setAttribute('src', './assets/img/autumn/2.jpg');
+      pic3.setAttribute('src', './assets/img/autumn/3.jpg');
+      pic4.setAttribute('src', './assets/img/autumn/4.jpg');
+      pic5.setAttribute('src', './assets/img/autumn/5.jpg');
+      pic6.setAttribute('src', './assets/img/autumn/6.jpg');
+      autumn.classList.add('portfolio__btn_changeColor');
+      winter.classList.remove('portfolio__btn_changeColor');
+      spring.classList.remove('portfolio__btn_changeColor');
+      summer.classList.remove('portfolio__btn_changeColor');
     }
   }
 });
 
 //Change theme
-let dark = document.querySelector("#dark_theme");
-let light = document.querySelector("#white_theme");
+let dark = document.querySelector('#dark_theme');
+let light = document.querySelector('#white_theme');
 
 function changeTheme() {
-  if (localStorage.getItem("theme") === "white") {
-    localStorage.removeItem("theme");
+  if (localStorage.getItem('theme') === 'white') {
+    localStorage.removeItem('theme');
   } else {
-    localStorage.setItem("theme", "white");
+    localStorage.setItem('theme', 'white');
   }
   addWhiteClass();
 }
 
 function addWhiteClass() {
   try {
-    if (localStorage.getItem("theme") === "white") {
-      dark.classList.add("header__theme_hidden");
-      dark.classList.remove("header__theme");
-      light.classList.add("header__theme");
-      light.classList.remove("header__theme_hidden");
-      document.querySelector("body").classList.add("white");
+    if (localStorage.getItem('theme') === 'white') {
+      dark.classList.add('header__theme_hidden');
+      dark.classList.remove('header__theme');
+      light.classList.add('header__theme');
+      light.classList.remove('header__theme_hidden');
+      document.querySelector('body').classList.add('white');
     } else {
-      dark.classList.add("header__theme");
-      dark.classList.remove("header__theme_hidden");
-      light.classList.add("header__theme_hidden");
-      light.classList.remove("header__theme");
+      dark.classList.add('header__theme');
+      dark.classList.remove('header__theme_hidden');
+      light.classList.add('header__theme_hidden');
+      light.classList.remove('header__theme');
 
-      document.querySelector("body").classList.remove("white");
+      document.querySelector('body').classList.remove('white');
     }
   } catch (err) {}
 }
 addWhiteClass();
+//button click
+const buttonClick = document.querySelectorAll('.ripple');
+for (let item of buttonClick) {
+  item.addEventListener('click', function (e) {
+    const x = e.clientX + window.scrollX;
+    const y = e.clientY + window.scrollY;
+
+    const xInside = x;
+    const yInside = y;
+
+    const circle = document.createElement('span');
+    circle.classList.add('circle');
+    circle.style.top = yInside + 'px';
+    circle.style.left = xInside + 'px';
+
+    this.appendChild(circle);
+
+    setTimeout(() => circle.remove(), 500);
+  });
+}
